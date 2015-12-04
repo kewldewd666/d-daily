6fc2c0c3235a34fa3b77399f747be9f25ab764de
jeff <jeffdavidthompson@gmail.com>
2015-12-03 16:03:48 -0800

    PIP8 and DRY gspread file

--- test-gspread.py
+++ test-gspread.py
@@ -9,51 +9,44 @@ alpha_employees = OrderedDict(sorted(employee_hours.items()))
 json_key = json.load(open('ddaily-982286e5cb7b.json'))
 scope = ['https://spreadsheets.google.com/feeds']
 
-credentials = SignedJwtAssertionCredentials(json_key['client_email'], json_key['private_key'].encode(), scope)
+credentials = SignedJwtAssertionCredentials(json_key['client_email'],
+                                            json_key['private_key'].encode(),
+                                            scope)
 
 gc = gspread.authorize(credentials)
 
 wks = gc.open("test-sheet").sheet1
 
-'''
-Update Names
-'''
-# for key, value in alpha_employees.iteritems():
-#     print key
 
-#print alpha_employees
+'''
+Update Names (doesn't want to work in a fucntion)
+'''
 
-num = 1
+
+num = 2
 for key, value in alpha_employees.iteritems():
     wks.update_cell(num, 2, key)
     num += 1
 
-num = 1
-for key, value in alpha_employees.iteritems():
-    wks.update_cell(num, 3, value[0])
-    num += 1
+'''
+writes down ONE column from an ordered dictionary with lists,
+starting at a specific index of the lists
+'''
 
-num = 1
-for key, value in alpha_employees.iteritems():
-    wks.update_cell(num, 4, value[1])
-    num += 1
 
-num = 1
-for key, value in alpha_employees.iteritems():
-    wks.update_cell(num, 5, value[2])
-    num += 1
+def write_col_from_dic_wlists(dic, start_row, start_col, list_index):
+    for key, value in dic.iteritems():
+        wks.update_cell(start_row, start_col, value[list_index])
+        start_row += 1
 
-num = 1
-for key, value in alpha_employees.iteritems():
-    wks.update_cell(num, 6, value[3])
-    num += 1
 
-num = 1
-for key, value in alpha_employees.iteritems():
-    wks.update_cell(num, 7, value[4])
-    num += 1
+def write_many_columns_of_dic_wlists(dic, start_row, start_col):
+    i = 0
+    list_index = 0
+    while i < len(dic.items()[0][1]):
+        write_col_from_dic_wlists(dic, start_row, start_col, list_index)
+        list_index += 1
+        start_col += 1
+        i += 1
 
-num = 1
-for key, value in alpha_employees.iteritems():
-    wks.update_cell(num, 8, value[5])
-    num += 1
+write_many_columns_of_dic_wlists(alpha_employees, 2, 3)
