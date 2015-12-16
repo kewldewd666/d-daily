8c6e04bc9945e8568a350b266896990f6787f364
Jeff <jeffdavidthompson@gmail.com>
2015-12-16 15:09:51 -0800

    fix and complete part 5

--- sitetutorial/mysite/polls/tests.py
+++ sitetutorial/mysite/polls/tests.py
@@ -1,6 +1,3 @@
-from django.test import TestCase
-
-# Create your tests here.
 import datetime
 
 from django.utils import timezone
@@ -8,6 +5,17 @@ from django.test import TestCase
 
 from .models import Question
 from django.core.urlresolvers import reverse
+
+def create_question(question_text, days):
+    """
+    Creates a question with the given `question_text` and published the
+    given number of `days` offset to now (negative for questions published
+    in the past, positive for questions that have yet to be published).
+    """
+    time = timezone.now() + datetime.timedelta(days=days)
+    return Question.objects.create(question_text=question_text,
+                                   pub_date=time)
+
 
 class QuestionMethodTests(TestCase):
 
@@ -37,17 +45,6 @@ class QuestionMethodTests(TestCase):
         time = timezone.now() - datetime.timedelta(hours=1)
         recent_question = Question(pub_date=time)
         self.assertEqual(recent_question.was_published_recently(), True)
-
-def create_question(question_text, days):
-    """
-    Creates a question with the given `question_text` and published the
-    given number of `days` offset to now (negative for questions published
-    in the past, positive for questions that have yet to be published).
-    """
-    time = timezone.now() + datetime.timedelta(days=days)
-    return Question.objects.create(question_text=question_text,
-                                   pub_date=time)
-
 
 class QuestionViewTests(TestCase):
     def test_index_view_with_no_questions(self):
@@ -106,3 +103,28 @@ class QuestionViewTests(TestCase):
             response.context['latest_question_list'],
             ['<Question: Past question 2.>', '<Question: Past question 1.>']
         )
+
+
+class QuestionIndexDetailTests(TestCase):
+    def test_detail_view_with_a_future_question(self):
+        """
+        The detail view of a question with a pub_date in the future should
+        return a 404 not found.
+        """
+        future_question = create_question(question_text='Future question.',
+                                          days=5)
+        response = self.client.get(reverse('polls:detail',
+                                   args=(future_question.id,)))
+        self.assertEqual(response.status_code, 404)
+
+    def test_detail_view_with_a_past_question(self):
+        """
+        The detail view of a question with a pub_date in the past should
+        display the question's text.
+        """
+        past_question = create_question(question_text='Past Question.',
+                                        days=-5)
+        response = self.client.get(reverse('polls:detail',
+                                   args=(past_question.id,)))
+        self.assertContains(response, past_question.question_text,
+                            status_code=200)
--- sitetutorial/mysite/polls/views.py
+++ sitetutorial/mysite/polls/views.py
@@ -2,10 +2,9 @@ from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.views import generic
-from django.utils import timezone
 
 from .models import Choice, Question
-
+from django.utils import timezone
 
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
@@ -16,62 +15,35 @@ class IndexView(generic.ListView):
         Return the last five published questions (not including those set to be
         published in the future).
         """
-        return Question.objects.filter(
-            pub_date__lte=timezone.now()
-        ).order_by('-pub_date')[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
-
+    model = Question
+    template_name = 'polls/detail.html'
     def get_queryset(self):
         """
         Excludes any questions that aren't published yet.
         """
         return Question.objects.filter(pub_date__lte=timezone.now())
 
-
 class ResultsView(generic.DetailView):
     model = Question
     template_name = 'polls/results.html'
 
-    def vote(request, question_id):
-        question = get_object_or_404(Question, pk=question_id)
-        try:
-            selected_choice = question.choice_set.get(pk=request.POST['choice'])
-        except (KeyError, Choice.DoesNotExist):
-            # Redisplay the question voting form.
-            return render(request, 'polls/detail.html', {
-                'question': question,
-                'error_message': "You didn't select a choice.",
-            })
-        else:
-            selected_choice.votes += 1
-            selected_choice.save()
-            # Always return an HttpResponseRedirect after successfully dealing
-            # with POST data. This prevents data from being posted twice if a
-            # user hits the Back button.
-            return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
-
-class QuestionIndexDetailTests(TestCase):
-
-    def test_detail_view_with_a_future_question(self):
-        """
-        The detail view of a question with a pub_date in the future should
-        return a 404 not found.
-        """
-        future_question = create_question(question_text='Future question.',
-                                          days=5)
-        response = self.client.get(reverse('polls:detail',
-                                   args=(future_question.id,)))
-        self.assertEqual(response.status_code, 404)
-
-    def test_detail_view_with_a_past_question(self):
-        """
-        The detail view of a question with a pub_date in the past should
-        display the question's text.
-        """
-        past_question = create_question(question_text='Past Question.',
-                                        days=-5)
-        response = self.client.get(reverse('polls:detail',
-                                   args=(past_question.id,)))
-        self.assertContains(response, past_question.question_text,
-                            status_code=200)
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
